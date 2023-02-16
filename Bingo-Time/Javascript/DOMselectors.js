5b9651c674955a64a36786c9421e461ac10c0cce
TylerBo12 <Tylershovel@gmail.com>
2023-02-15 22:43:38 -0500

    Mass confusion in my brain

--- Bingo-Time/Javascript/DOMselectors.js
+++ Bingo-Time/Javascript/DOMselectors.js
@@ -1,6 +1,9 @@
 const DOMSelectors = {
   parent: document.querySelector("#parent"),
   child: document.querySelector(".child"),
+  pickNumber: document.querySelector(".pickNumber"),
+  generateCard: document.querySelector(".generateCard"),
+  buttonContainer: document.querySelector("#buttonContainer"),
 };
 
 export { DOMSelectors };
--- Bingo-Time/Javascript/main.js
+++ Bingo-Time/Javascript/main.js
@@ -3,13 +3,45 @@ import "../Styles/variables.css";
 import { DOMSelectors } from "./DOMselectors";
 import { bingoCard } from "./array";
 
-const bingoBalls = function () {
-  bingoCard.card1.forEach((number) => {
-    DOMSelectors.parent.insertAdjacentHTML(
-      "beforeend",
-      '<sub class="child">1</sub>'
-    );
-  });
+const partsOfBingo = {
+  bingoBalls: function () {
+    bingoCard.card1.forEach((number) => {
+      DOMSelectors.parent.insertAdjacentHTML(
+        "beforeend",
+        `<sub class="child">${partsOfBingo.generateRandom(90)}</sub>`
+      );
+    });
+  },
+
+  generateRandom: function (max) {
+    const randomInteger = Math.floor(Math.random() * max);
+    /* console.log(randomInteger); */
+    return randomInteger;
+  },
+
+  randomNumber: function () {
+    const roll = partsOfBingo.generateRandom(90);
+    console.log(roll);
+    if (roll === partsOfBingo.generateRandom(90)) {
+      alert("Hello");
+    }
+  },
+
+  noDupes: function () {
+    if (partsOfBingo.generateRandom(90) === 1) {
+      this.removeNumbers();
+    }
+  },
+
+  removeNumbers: function () {
+    const numbers = document.querySelectorAll(".child");
+    const numbersArray = Array.from(numbers);
+    numbersArray.forEach((number) => {
+      number.remove();
+    });
+  },
 };
 
-bingoBalls();
+partsOfBingo.randomNumber();
+partsOfBingo.bingoBalls();
+partsOfBingo.noDupes();
